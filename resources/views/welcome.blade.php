6bf8d08bf905aad18bfc4b758b591bfab2c3150f
kanu peace <kanupeace85@gmail.com>
2020-12-24 04:45:58 -0800

    Saving state

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,10 +85,10 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="/brandfor">brand</a>
+                    <a href="/brand">brand4freevote</a>
+                    <a href="/reg">reg_forfree</a>
+                    <a href="/printoption">printoption</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>

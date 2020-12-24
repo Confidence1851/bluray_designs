98968eea21fc80e73ae3de542867281cc92916e6
kanu peace <kanupeace85@gmail.com>
2020-12-24 04:16:44 -0800

    added brand pages

---
+++ app/Http/Controllers/reg_brandforfreecontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class reg_brandforfreecontroller extends Controller
+{
+    public function reg_brandforfree () {
+
+        return view('admin.reg_brandforfree');
+    }
+}
---
+++ resources/views/admin/brandforfree.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+    <body background="img/people-mover.jpg" style="">
+        <div class="container" style="padding-top:3%;">
+        <!--welcome to bluraydesign brand4free initiative -->
+        <div class="row">
+        <div class="col-md-12">
+        <p style="padding-left:3%; margin-bottom:4%; padding-right:3%;"></p><center><img src="img/brand4free-logo.png" class=" img-fluid" alt="Brand4free logo"></center><p></p>
+        <h2 style="text-align:center; color:#666; font-family: Century Gothic, Arial, Tahoma; font-size:24px;">WELCOME TO BLURAY DESIGNTECH'S<br> <span style="font-size:40px; color:#036;"> <b><i>BRAND4FREE</i></b> INITIATIVE</span></h2>
+        </div>
+        </div>
+
+
+        <!--picture and intro section -->
+        <div class="row" style="margin-top:5%; margin-bottom:10%; color:#666;:">
+
+        <div class="col-md-6 col-sm-6 col-lg-6">
+        <img class="img-fluid" alt="The Entreprenuer" src="img/entre.png">
+        </div>
+
+
+
+
+        <div class="clearfix"></div>
+        <div class="col-md-5 col-sm-5 col-lg-5" style="text-align:justify">
+        <hr>
+        <p>As a corporate branding agency, we are passionate about helping businesses; particularly SMEs grow their brands through print/digital branding services.
+        </p><p>
+        Each month, we give two (2) SMEs with highest votes accumulated after a 3 day voting period free Designs/Prints to help brand and grow their business. Ten(10) slots are opened on the first day of each month, participants' application are screened and the top ten enters the voting poll.
+        </p>
+          <p></p>
+
+        <p>Will you like to participate? create an account and login to get started...</p>
+
+        <p>
+        <button type="button" class="btn btn-primary"> Get Started</button>
+        </p>
+        </div>
+
+
+        </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
+              <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+
+
+</body>
+</html>

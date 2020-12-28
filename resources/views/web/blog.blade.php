e9dd72e1eb26e5e95f256b5a1a9784943b1001a3
Confidence Ugolo <ugoloconfidence@gmail.com>
2020-12-28 20:03:08 +0100

    Saving state

--- resources/views/web/blog.blade.php
+++ resources/views/web/blog.blade.php
@@ -2,104 +2,96 @@
 
 @section('content')
     <section id="inner-headline">
-        <div class="container">
-            <div class="row">
-                <div class="span4">
-                    <div class="inner-heading">
-                        <h2>Our Blog</h2>
-                    </div>
-                </div>
-                <div class="span8">
-                    <ul class="breadcrumb">
-                        <li><a href="{{ url('/') }}"><i class="icon-home"></i></a><i class="icon-angle-right"></i></li>
-                        <li><a href="#">Blog</a></li>
-                    </ul>
-                </div>
+      <div class="container">
+        <div class="row">
+          <div class="span4">
+            <div class="inner-heading">
+              <h2>Our Blog</h2>
             </div>
+          </div>
+          <div class="span8">
+            <ul class="breadcrumb">
+              <li><a href="{{url('/')}}"><i class="icon-home"></i></a><i class="icon-angle-right"></i></li>
+              <li><a href="#">Blog</a></li>
+            </ul>
+          </div>
         </div>
+      </div>
     </section>
     <section id="content">
-        <div class="container">
-            <div class="row">
+      <div class="container">
+        <div class="row">
+          <div class="span8">
+          @foreach($posts as $post)
+            @php($cmts = App\Comment::where('post_id',$post->id)->count())
+            <article>
+              <div class="row">
                 <div class="span8">
-                    @foreach ($posts as $post)
-                        @php($cmts = App\Comment::where('post_id', $post->id)->count())
-                            <article>
-                                <div class="row">
-                                    <div class="span8">
-                                        <div class="post-image">
-                                            <div class="post-heading">
-                                                <h3><a href="javascript:void(0)">{{ $post->title }}</a></h3>
-                                            </div>
-                                            <img src="{{ asset('public/post_images/' . $post->image) }}" alt="" />
-                                        </div>
-                                        <p>
-                                        </p>
-                                        <div class="bottom-article">
-                                            <ul class="meta-post">
-                                                <li><i class="icon-calendar"></i><a href="#">
-                                                        {{ date('D, M d Y', strtotime($post->created_at)) }}</a></li>
-                                                <li><i class="icon-user"></i><a href="javascript:void(0)"> Admin</a></li>
-                                                <li><i class="icon-folder-open"></i><a
-                                                        href="javascript:void(0)">{{ $post->category }}</a></li>
-                                                <li><i class="icon-comments"></i><a href="javascript:void(0)">{{ $cmts }}
-                                                        Comments</a></li>
-                                            </ul>
-                                            <a href="{{ route('blogpost', ['id' => $post->id, 'slug' => $post->slug]) }}"
-                                                class="pull-right">Continue reading <i class="icon-angle-right"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </article>
-                        @endforeach
-
-                        <div id="pagination">
-                            <span class="all">Page 1 of 3</span>
-                            <span class="current">1</span>
-                            <a href="#" class="inactive">2</a>
-                            <a href="#" class="inactive">3</a>
-                        </div>
+                  <div class="post-image">
+                    <div class="post-heading">
+                      <h3><a href="javascript:void(0)">{{$post->title}}</a></h3>
                     </div>
-                    <div class="span4">
-                        <aside class="right-sidebar">
-                            <div class="widget">
-                                <form class="form-search">
-                                    <input placeholder="Type something" type="text" class="input-medium search-query">
-                                    <button type="submit" class="btn btn-square btn-theme">Search</button>
-                                </form>
-                            </div>
-                            <div class="widget">
-                                <h5 class="widgetheading">Categories</h5>
-                                <ul class="cat">
-                                    @foreach ($post_cats as $cat)
-                                        @php($count = App\Post::where('category', $cat)->count())
-                                            <li><i class="icon-angle-right"></i><a href="#">{{ $cat }}</a><span>
-                                                    ({{ $count }})</span></li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                                <div class="widget">
-                                    <h5 class="widgetheading">Latest posts</h5>
-                                    <ul class="recent">
-                                        @foreach ($latests as $latest)
-                                            <li>
-                                                <img src="{{ asset('public/post_images/' . $latest->image) }}" class="pull-left"
-                                                    alt="{{ $latest->title }}" />
-
-                                                <h6><a
-                                                        href="{{ route('blogpost', ['id' => $latest->id, 'slug' => $latest->slug]) }}">{{ $latest->title }}</a>
-                                                </h6>
-
-                                                <!-- <p>
-                                  Mazim alienum appellantur eu cu ullum officiis pro pri
-                                </p> -->
-                                            </li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            </aside>
-                        </div>
-                    </div>
+                    <img src="{{ asset('public/post_images/'.$post->image ) }}" alt="" />
+                  </div>
+                  <p>
+                  </p>
+                  <div class="bottom-article">
+                    <ul class="meta-post">
+                      <li><i class="icon-calendar"></i><a href="#"> {{date('D, M d Y',strtotime($post->created_at))}}</a></li>
+                      <li><i class="icon-user"></i><a href="javascript:void(0)"> Admin</a></li>
+                      <li><i class="icon-folder-open"></i><a href="javascript:void(0)">{{$post->category}}</a></li>
+                      <li><i class="icon-comments"></i><a href="javascript:void(0)">{{$cmts}} Comments</a></li>
+                    </ul>
+                    <a href="{{ route('blogpost',['id'=> $post->id,'slug' => $post->slug]) }}" class="pull-right">Continue reading <i class="icon-angle-right"></i></a>
+                  </div>
                 </div>
-            </section>
-        @stop
+              </div>
+            </article>
+            @endforeach
+
+            <div id="pagination">
+              <span class="all">Page 1 of 3</span>
+              <span class="current">1</span>
+              <a href="#" class="inactive">2</a>
+              <a href="#" class="inactive">3</a>
+            </div>
+          </div>
+          <div class="span4">
+            <aside class="right-sidebar">
+              <div class="widget">
+                <form class="form-search">
+                  <input placeholder="Type something" type="text" class="input-medium search-query">
+                  <button type="submit" class="btn btn-square btn-theme">Search</button>
+                </form>
+              </div>
+              <div class="widget">
+                <h5 class="widgetheading">Categories</h5>
+                <ul class="cat">
+                  @foreach($post_cats as $cat)
+                    @php($count = App\Post::where('category',$cat)->count())
+                    <li><i class="icon-angle-right"></i><a href="#">{{$cat}}</a><span> ({{$count}})</span></li>
+                  @endforeach
+                </ul>
+              </div>
+              <div class="widget">
+                <h5 class="widgetheading">Latest posts</h5>
+                <ul class="recent">
+                  @foreach($latests as $latest)
+                  <li>
+                    <img src="{{ asset('public/post_images/'.$latest->image ) }}" class="pull-left" alt="{{$latest->title}}" />
+
+                      <h6><a href="{{ route('blogpost',['id'=> $latest->id,'slug' => $latest->slug]) }}">{{$latest->title}}</a></h6>
+
+                    <!-- <p>
+                      Mazim alienum appellantur eu cu ullum officiis pro pri
+                    </p> -->
+                  </li>
+                  @endforeach
+                </ul>
+              </div>
+            </aside>
+          </div>
+        </div>
+      </div>
+    </section>
+  @stop
--- resources/views/web/products.blade.php
+++ resources/views/web/products.blade.php
@@ -2,204 +2,204 @@
 
 @section('content')
 
-    <style>
-        @import url(http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css);
+<style>
+            
+            @import url(http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css);
+.col-item
+{
+    border: 1px solid #E1E1E1;
+    border-radius: 5px;
+    background: #FFF;
+	font-family:Tahoma, Geneva, sans-serif;
+	margin-bottom:8%;
+}
+.col-item .photo img
+{
+    margin: 0 auto;
+    width: 100%;
+}
 
-        .col-item {
-            border: 1px solid #E1E1E1;
-            border-radius: 5px;
-            background: #FFF;
-            font-family: Tahoma, Geneva, sans-serif;
-            margin-bottom: 8%;
-        }
+.h6adjust
+{ font-size:14px;
+margin-top:0px;
+padding-top:0px;
+font-style:italic;
 
-        .col-item .photo img {
-            margin: 0 auto;
-            width: 100%;
-        }
+color:#C06;
 
-        .h6adjust {
-            font-size: 14px;
-            margin-top: 0px;
-            padding-top: 0px;
-            font-style: italic;
+	}
+	
+	h5
+	{
+		font-size:16px;
+		margin-top:0px;
+font-weight:bold;
+padding-top:0px;
+margin-bottom:0px;
+color:#333;
+	}
 
-            color: #C06;
+.col-item .info
+{
+    padding: 10px;
+    border-radius: 0 0 5px 5px;
+    margin-top: 1px;
+}
 
-        }
+.col-item:hover .info {
+    background-color: #F5F5DC;
+}
+.col-item .price
+{
+    /*width: 50%;*/
+    float: left;
+    margin-top: 5px;
+}
 
-        h5 {
-            font-size: 16px;
-            margin-top: 0px;
-            font-weight: bold;
-            padding-top: 0px;
-            margin-bottom: 0px;
-            color: #333;
-        }
+.col-item .price h6
+{
+    line-height: 20px;
+    margin: 0;
+}
 
-        .col-item .info {
-            padding: 10px;
-            border-radius: 0 0 5px 5px;
-            margin-top: 1px;
-        }
+.price-text-color
+{
+    color:#C06;
+}
 
-        .col-item:hover .info {
-            background-color: #F5F5DC;
-        }
-
-        .col-item .price {
-            /*width: 50%;*/
-            float: left;
-            margin-top: 5px;
-        }
-
-        .col-item .price h6 {
-            line-height: 20px;
-            margin: 0;
-        }
-
-        .price-text-color {
-            color: #C06;
-        }
-
-        .col-item .info .rating {
-            color: #777;
-        }
-
-
-
-        .col-item .separator {
-            border-top: 1px solid #E1E1E1;
-        }
-
-        .clear-left {
-            clear: left;
-        }
-
-        .col-item .separator p {
-            line-height: 20px;
-            margin-bottom: 0;
-            margin-top: 10px;
-            text-align: center;
-        }
-
-        .col-item .separator p i {
-            margin-right: 5px;
-        }
-
-        .col-item .btn-add {
-            width: 50%;
-            float: left;
-        }
-
-        .col-item .btn-add {
-            border-right: 1px solid #E1E1E1;
-        }
-
-        .col-item .btn-details {
-            width: 50%;
-            float: left;
-            padding-left: 10px;
-        }
-
-        .controls {
-            margin-top: 20px;
-        }
-
-        [data-slide="prev"] {
-            margin-right: 10px;
-        }
-
-    </style>
-    <section>
-        <div class="container wow fadeInUp" style="margin-left:7%">
-            <div class="row">
-                <div class="row" style="margin-bottom:0px; padding-top:7%;">
-                    <div class="span12" style=" margin-bottom:0px;">
-
-                        <h4 class="text-center" style="color:#036; font-family:Century Gothic, Tahoma, Geneva, sans-serif">
-                            ---- OUR PRODUCTS ----</h4>
-                    </div>
-
-                </div>
-                <div id="carousel-example" class="carousel slide hidden-xs" data-ride="carousel">
-                    <!-- Wrapper for slides -->
-                    <a href="#">
-                        <div class="carousel-inner">
-                            <div class="item active">
-                                <div class="row products_list">
-
-
-                                    @foreach ($products as $product)
-                                        @php($last_img = App\Picture::where('product_id', $product->id)
-                                            ->orderby('created_at', 'desc')
-                                            ->first(),)
-                                            @php($quan = App\Quantity::where('product_id', $product->id)
-                                                ->orderby('quantity', 'asc')
-                                                ->first(),)
-                                                @php($type = App\Type::where('product_id', $product->id)
-                                                    ->orderby('price', 'asc')
-                                                    ->first(),)
-                                                    @php($low_price = $quan->quantity * $type->price - $quan->discount)
-
-                                                        <div class="span3">
-                                                            <a href="{{ route('productdetail', $product->id) }}">
-                                                                <div class="col-item">
-                                                                    <div class="photo">
-                                                                        <img src="{{ asset('public/product_images/' . $last_img->image) }}"
-                                                                            class="img-responsive" alt="Order a Website Design" />
-                                                                    </div>
-                                                                    <div class="info">
-                                                                        <div class="row" style="margin-bottom:10px;">
-                                                                            <div class="price span6 " style="margin-bottom:0px;">
-                                                                                <h5>{{ strtoupper($product->name) }}</h5>
-                                                                                <h6 class="price-text-color h6adjust">
-                                                                                    from <b class="product_price">{{ $low_price }}</b> |
-                                                                                    {{ $product->caption }}</h6>
-                                                                            </div>
-
-                                                                        </div>
-
-                                                                        <div class="row">
-                                                                            <div class="text-center"
-                                                                                style="margin-top:0px; padding-top:0px;">
-
-
-                                                                                <p style="margin-top:0px;">
+.col-item .info .rating
+{
+    color: #777;
+}
 
 
 
-                                                                                    <button class="btn btn-lg btn-primary"
-                                                                                        style="background-color:#0275d8"> ORDER NOW
-                                                                                    </button>
-                                                            </a>
-                                                            </p>
+.col-item .separator
+{
+    border-top: 1px solid #E1E1E1;
+}
 
+.clear-left
+{
+    clear: left;
+}
 
-                                                        </div>
-                                                </div>
-                                                <div class="clearfix">
-                                                </div>
-                                            </div>
-                                        </div>
+.col-item .separator p
+{
+    line-height: 20px;
+    margin-bottom: 0;
+    margin-top: 10px;
+    text-align: center;
+}
+
+.col-item .separator p i
+{
+    margin-right: 5px;
+}
+.col-item .btn-add
+{
+    width: 50%;
+    float: left;
+}
+
+.col-item .btn-add
+{
+    border-right: 1px solid #E1E1E1;
+}
+
+.col-item .btn-details
+{
+    width: 50%;
+    float: left;
+    padding-left: 10px;
+}
+.controls
+{
+    margin-top: 20px;
+}
+[data-slide="prev"]
+{
+    margin-right: 10px;
+}
+            </style>
+            <section>
+            <div class="container wow fadeInUp" style="margin-left:7%">
+             <div class="row">
+        <div class="row" style="margin-bottom:0px; padding-top:7%;">
+            <div class="span12" style=" margin-bottom:0px;">
+            
+                <h4 class="text-center" style="color:#036; font-family:Century Gothic, Tahoma, Geneva, sans-serif">
+                    ---- OUR PRODUCTS ----</h4>
+            </div>
+          
+        </div>
+        <div id="carousel-example" class="carousel slide hidden-xs" data-ride="carousel">
+            <!-- Wrapper for slides -->
+            <a href="#">
+            <div class="carousel-inner">
+                <div class="item active">
+                    <div class="row products_list">
+                    
+                    
+                    @foreach($products as $product)
+                      @php($last_img = App\Picture::where('product_id',$product->id)->orderby('created_at','desc')->first())
+                      @php($quan = App\Quantity::where('product_id',$product->id)->orderby('quantity','asc')->first())
+                      @php($type = App\Type::where('product_id',$product->id)->orderby('price','asc')->first())
+                      @php($low_price = (($quan->quantity * $type->price) - $quan->discount) )
+                      
+                     <div class="span3">
+                        <a href="{{ route('productdetail',$product->id)}}">
+                            <div class="col-item">
+                                <div class="photo">
+                                    <img src="{{ asset('public/product_images/'.$last_img->image) }}" class="img-responsive" alt="Order a Website Design" />
                                 </div>
-                                @endforeach
-
-
-
-
+                                <div class="info">
+                                    <div class="row" style="margin-bottom:10px;">
+                                        <div class="price span6 " style="margin-bottom:0px;">
+                                            <h5>{{strtoupper($product->name)}}</h5>
+                                            <h6 class="price-text-color h6adjust">
+                                                 from <b class="product_price" >{{$low_price}}</b> | {{$product->caption}}</h6>
+                                        </div>
+                                     
+                                    </div>
+                                    
+                                    <div class="row">
+                                    <div class="text-center" style="margin-top:0px; padding-top:0px;">
+                                  
+                                   
+                                    <p style="margin-top:0px;" >
+                        
+                                    
+                                
+                                 <button class="btn btn-lg btn-primary" style="background-color:#0275d8" >  ORDER NOW </button>
+                                  </a>
+                                    </p>
+                                 
+                                        
+                                    </div>
+                                    </div>
+                                    <div class="clearfix">
+                                    </div>
+                                </div>
                             </div>
                         </div>
+                       @endforeach
+                    
+                    
+                       
+                       
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+ </div>
+   </section>
+   
+   
+   
+   
 
-                        </div>
-                        </div>
-                        </div>
-                        </div>
-                    </section>
-
-
-
-
-
-
-                @stop
+    
+@stop  
